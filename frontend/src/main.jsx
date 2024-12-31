545cba86b2eb12bd6e36e55c6339148ca5340fbc
brynsgtn <bryansuguitan17@gmail.com>
2024-12-31 10:50:18 -0600

    code along completed

---
+++ frontend/src/main.jsx
@@ -0,0 +1,43 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider
+} from 'react-router-dom';
+import store from './store.js';
+import { Provider } from 'react-redux';
+import './index.css';
+import PrivateRoute from './components/PrivateRoute.jsx';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import App from './App.jsx'
+import HomeScreen from './screens/HomeScreen.jsx';
+import LoginScreen from './screens/LoginScreen.jsx';
+import RegisterScreen from './screens/RegisterScreen.jsx';
+import ProfileScreen from './screens/ProfileScreen.jsx';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App />}>
+      <Route index={true} path='/' element={<HomeScreen />} />
+      <Route path='/login' element={<LoginScreen />} />
+      <Route path='/register' element={<RegisterScreen />} />
+      {/* Private Routes */}
+      <Route path='' element={<PrivateRoute />}>
+        <Route path='/profile' element={<ProfileScreen />} />
+      </Route>
+    </Route>
+  )
+)
+
+createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
+    <StrictMode>
+      <RouterProvider router={router} future={{
+        v7_relativeSplatPath: true,
+        v7_startTransition: true
+      }} />
+    </StrictMode>,
+  </Provider>
+)
